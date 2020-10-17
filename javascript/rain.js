08fd857a09a1fedfa3c5c4030f84b9e0b7b5546f
michellenicole23 <64987167+michellenicole23@users.noreply.github.com>
2020-10-16 22:39:29 -0500

    fixed mouseIsPressed, added array of colors.

--- javascript/rain.js
+++ javascript/rain.js
@@ -1,22 +1,29 @@
 let rain = new p5(( sketch ) => {
     let xPositions = [200];
     let yPositions = [0];   
-    
+    let rColor = [];
+    let bColor = [];
+    let gColor = [];
+
     sketch.setup = () => {
       sketch.createCanvas(width, height);
     };
   
     sketch.draw = () => {
-        if(sketch.mouseClicked) {
-            xPositions.sketch.push(sketch.mouseX);
-            yPositions.sketch.push(sketch.mouseY);
-            sketch.fill(sketch.random(0, 255), sketch.random(0, 255), sketch.random(0, 255));
+        if(sketch.mouseIsPressed) {
+            xPositions.push(sketch.mouseX);
+            yPositions.push(sketch.mouseY);
+            rColor.push(sketch.random(0,255));
+            bColor.push(sketch.random(0,255));
+            gColor.push(sketch.random(0,255));
+            
         }
         sketch.background(16, 18, 43);
         sketch.noStroke();
        
         
         for (var i = 0; i < xPositions.length; i++) {
+            sketch.fill(rColor[i], bColor[i], gColor[i]);
             sketch.ellipse(xPositions[i], yPositions[i] % 400, 10, 10);
             yPositions[i] += 5;
         }
