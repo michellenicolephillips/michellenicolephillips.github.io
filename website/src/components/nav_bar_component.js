9eec8cd857af574cb80cf259fb07b094de976b7f
michellenicole23 <64987167+michellenicole23@users.noreply.github.com>
2021-01-22 14:12:02 -0600

    added react-router-dom to route nav bar

--- website/src/components/nav_bar_component.js
+++ website/src/components/nav_bar_component.js
@@ -1,16 +1,48 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './nav_bar_component.css';
 
 class NavBar extends React.Component {
   render() {
     return ( 
-    <ul className="navbar">
-        <li className="navlink" id="active"><a href="index.html">Home</a></li>
-        <li className="navlink"><a href="projects.html">Projects</a></li>
-        <li className="navlink"><a href="resume.html">Resume</a></li>
-    </ul>
+    <Router>
+      <div>
+        <ul className="navbar">
+            <li className="navlink" id="active"><Link to="/">Home</Link></li>
+            <li className="navlink"><Link to="/projects">Projects</Link></li>
+            <li className="navlink"><Link to="/resume">Resume</Link></li>
+        </ul>
+        <Switch>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     );
   }
+}
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Projects() {
+  return <h2>Projects</h2>;
+}
+
+function Resume() {
+  return <h2>Resume</h2>;
 }
 
 export default NavBar;
