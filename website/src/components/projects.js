512d1d64c0cf693099613421242988b4f4c1d96e
michellenicole23 <64987167+michellenicole23@users.noreply.github.com>
2021-02-01 17:14:31 -0600

    added nav bar to switch through projects vs all being displayed at once, added projects css to new nav bar

--- website/src/components/projects.js
+++ website/src/components/projects.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import './projects.css'; 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink
+  } from "react-router-dom";
 import Bookshelf from './p5js-components/bookshelf';
 import Rain from './p5js-components/rain';
 import House from './p5js-components/house';
@@ -14,22 +21,58 @@ import Plate from './p5js-components/plate';
 class Projects extends React.Component {
   render() {
     return (
-            <div> 
-                <p className="content">
+        <div>
+          <p className="content">
                     This page contains every project I've work on in my journey to learning to code from the latest project I've worked on to the very first one. <br/>
                     I started with<a className="links "href="https://www.codecademy.com/" target="_blank" rel="noreferrer"><strong>Codecademy</strong></a>for CSS/HTML and moved to<a className= "links" href="https://www.khanacademy.org/" target="_blank" rel="noreferrer"><strong>Khan Academy</strong></a>for Javascript and SQL.<br/>
                     I will continue to add to this page as I learn more languages. Currently I am learning C# and React.
                 </p>
-                <Bookshelf/>
-                <Rain/>
-                <House/>
-                <Magic8/>
-                <Fishtank/>
-                <Spork/>
-                <Animal/>
-                <City/>
-                <Plate/>
-            </div>
+        <Router>
+      <div>
+        <ul className="projectbar">
+            <li className="projlink"><NavLink exact to="/bookshelf" activeClassName="active">Bookshelf</NavLink></li>
+            <li className="projlink"><NavLink to="/rain" activeClassName="active">Rain</NavLink></li>
+            <li className="projlink"><NavLink to="/house" activeClassName="active">House</NavLink></li>
+            <li className="projlink"><NavLink exact to="/magic8" activeClassName="active">Fortune Telling</NavLink></li>
+            <li className="projlink"><NavLink to="/fishtank" activeClassName="active">Fishtank</NavLink></li>
+            <li className="projlink"><NavLink to="/spork" activeClassName="active">Marketing</NavLink></li>
+            <li className="projlink"><NavLink to="/animal" activeClassName="active">Mouse</NavLink></li>
+            <li className="projlink"><NavLink to="/city" activeClassName="active">City Skyline</NavLink></li>
+            <li className="projlink"><NavLink to="/plate" activeClassName="active">What's for dinner?</NavLink></li>
+        </ul>
+        <Switch>
+          <Route path="/bookshelf">
+            <Bookshelf/>
+          </Route>
+          <Route path="/rain">
+            <Rain/>
+          </Route>
+          <Route path="/house">
+            <House/>
+          </Route>
+          <Route path="/magic8">
+            <Magic8/>
+          </Route>
+          <Route path="/fishtank">
+            <Fishtank/>
+          </Route>
+          <Route path="/spork">
+          <Spork/>
+          </Route>
+          <Route path="/animal">
+          <Animal/>
+          </Route>
+          <Route path="/city">
+          <City/>
+          </Route>
+          <Route path="/plate">
+          <Plate/>
+          </Route>
+          
+        </Switch>
+      </div>
+    </Router>
+        </div>
         );
     }
 }
