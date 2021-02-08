1083575da02cb8db62123c049e3b83c347962080
michellenicole23 <64987167+michellenicole23@users.noreply.github.com>
2021-02-08 12:40:41 -0600

    formatting and clean up

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,18 +3,18 @@ import './footer.css';
 import githublogo from '../res/images/github.png';
 import linkedinlogo from '../res/images/linkedin.png';
 
-
 class Footer extends React.Component {
-  render() {
-    return ( 
-        <footer className="footer"> 
-            <p> Check me out on:<br/> 
-                <a className="social" href="https://github.com/michellenicole23" target="_blank" rel="noreferrer"><img src= {githublogo} alt="github"/></a>
-                <a className="social" href="https://www.linkedin.com/in/michelle-phillips-8a56a764/" target="_blank" rel="noreferrer" ><img src={linkedinlogo} alt="linkedin"/></a>
-            </p>
-    </footer>
-    );
-  }
+     render() {
+          return ( 
+               <footer className="footer"> 
+                    <p>Check me out on:
+                         <br/> 
+                         <a className="social" href="https://github.com/michellenicole23" target="_blank" rel="noreferrer"><img src= {githublogo} alt="github"/></a>
+                         <a className="social" href="https://www.linkedin.com/in/michelle-phillips-8a56a764/" target="_blank" rel="noreferrer" ><img src={linkedinlogo} alt="linkedin"/></a>
+                    </p>
+               </footer>
+          );
+     }
 }
 
 export default Footer;
--- src/components/nav_bar_component.js
+++ src/components/nav_bar_component.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink
+     BrowserRouter as Router,
+     Switch,
+     Route,
+     NavLink
 } from "react-router-dom";
 import './nav_bar_component.css';
 import Home from './home';
@@ -11,31 +11,30 @@ import Resume from './resume';
 import Projects from './projects';
 
 class NavBar extends React.Component {
-  render() {
-    return ( 
-    <Router>
-      <div>
-        <ul className="navbar">
-            <li className="navlink"><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-            <li className="navlink"><NavLink to="/projects" activeClassName="active">Projects</NavLink></li>
-            <li className="navlink"><NavLink to="/resume" activeClassName="active">Resume</NavLink></li>
-        </ul>
-        <Switch>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/resume">
-            <Resume />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-    );
-  }
+     render() {
+          return ( 
+               <Router>
+                    <div>
+                         <ul className="navbar">
+                              <li className="navlink"><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+                              <li className="navlink"><NavLink exact to="/projects" activeClassName="active">Projects</NavLink></li>
+                              <li className="navlink"><NavLink exact to="/resume" activeClassName="active">Resume</NavLink></li>
+                         </ul>
+                         <Switch>
+                              <Route path="/projects">
+                                   <Projects />
+                              </Route>
+                              <Route path="/resume">
+                                   <Resume />
+                              </Route>
+                              <Route path="/">
+                                   <Home />
+                              </Route>
+                         </Switch>
+                    </div>
+               </Router>
+          );
+     }
 }
-
 
 export default NavBar;
