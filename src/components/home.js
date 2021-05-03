29fd1a416c9a35a17f8e2354555b3029027ae399
michellenicole23 <64987167+michellenicole23@users.noreply.github.com>
2021-05-03 18:33:06 -0500

    updated introduction on homepage

--- src/components/home.js
+++ src/components/home.js
@@ -8,12 +8,15 @@ class Home extends React.Component {
                     <head>
                          <title>Michelle P. - Home</title>
                     </head> 
-                    <h1>Welcome!</h1>
-                    <p className="intro">Hello, My name is Michelle!
-                         <br/>
-                         I come from the hospitality industry. Due to the covid-19 pandemic, I was let go.
-                         <br/>
-                         Thus started my journey in learning how to code.
+                    <h1>Hi Y'all!</h1>
+                    <h3>I'm Michelle</h3>
+                    <p className="intro">
+                         I'm a self-taught software developer coming from the hospitality industry.
+                         I have a strong background in client relations, have been a trainer in multiple roles, and took on projects outside of my comfort zone.
+                         In my previous roles I've always pushed past the definition of the job description,
+                         seeking to learn more.
+                         I've helped nurture agile and scrum culture in my teams and have broken down departmental silos, creating one cohesive team.
+                         While I come form a non-traditional background, I hope to be a great addition to the industry with the continuation of my technical education and agile and problem solving skills.
                          <br/>
                          So far I've learned a little of:
                     </p>
@@ -24,10 +27,12 @@ class Home extends React.Component {
                          <li>SQL</li>
                          <li>C#</li>
                          <li>React</li>
+                         <li>Java</li>
                     </ul>
                     <p className="intro">
-                         Currently working on Java.
-                         Follow along as I learn a new industry!
+                         Check out my projects page!
+                         Currently working on the Spring Framework.
+                         Follow along as I learn a new industry.
                     </p>
                </div>
           );
