ac33a2f5034f180f0792903f6b8216e6c3982fa7
michellenicole23 <64987167+michellenicole23@users.noreply.github.com>
2021-05-20 13:24:07 -0500

    made navbar and footer gradient colors, spacing on footer

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 personal website to show what I can do
 tracking progress as I learn how to code
 
-design inspiration from https://github.com/nordicgiant2/react-nice-resume
+design inspiration from https://github.com/nordicgiant2/react-nice-resume & https://github.com/cobidev/gatsby-simplefolio 
--- src/components/footer.js
+++ src/components/footer.js
@@ -10,16 +10,12 @@ import {Link} from 'react-scroll';
 class Footer extends React.Component {
      render() {
           return ( 
-               <footer className="footer">
+               <footer id="footer">
                     <Link to="home" smooth duration={1000}>
                          <img src = {caret} alt="arrow to scroll to top"/>
                     </Link>
-                   
-                    <p>Check me out on:
-                         <br/> 
                          <a className="icons" href="https://github.com/michellenicolephillips" target="_blank" rel="noreferrer"><img src= {githublogo} alt="github"/></a>
                          <a className="icons" href="https://www.linkedin.com/in/michellenicolephillips" target="_blank" rel="noreferrer" ><img src={linkedinlogo} alt="linkedin"/></a>
-                    </p>
                     
                </footer>
           );
