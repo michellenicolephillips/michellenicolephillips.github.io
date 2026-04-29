aa097f4b279c4ea33019062de4ccc21a6847506d
Michelle <64987167+michellenicolephillips@users.noreply.github.com>
2026-04-28 22:25:03 -0500

    change base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'michellenicolephillips.github.io/',
+  base: '/',
   // This helps Vite find files in your src folder properly
   resolve: {
     alias: {
